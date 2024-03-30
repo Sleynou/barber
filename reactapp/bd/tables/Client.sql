84f81b8bd0227b550863407871f2935b1d833e16
Jorge Espinal <e2296014@cmaisonneuve.qc.ca>
2024-03-30 14:00:42 +0000

    Update 4 files
    - /reactapp/bd/tables/Client.sql
    - /reactapp/bd/tables/SalonCoiffure.sql
    - /reactapp/bd/tables/Coiffeur.sql
    - /reactapp/bd/tables/Blacklist.sql

--- reactapp/bd/tables/Client.sql
+++ reactapp/bd/tables/Client.sql
@@ -1,8 +1,9 @@
 --Travail fait par Jorge/Alejandro
 create table Client(
-    idCLIENT int PRIMARY KEY IDENTITY(1,1), 
+    IDclient int PRIMARY KEY IDENTITY(1,1), 
+    UsernameClient varchar(50) unique,
     PrenomClient varchar(50) not null, 
     NomClient varchar(50) not null, 
     Email varchar(50) not null,
-    MotDePasse varchar(50) not null
+    MotDePasse varchar(128) not null
     )
--- reactapp/bd/tables/Coiffeur.sql
+++ reactapp/bd/tables/Coiffeur.sql
@@ -1,14 +1,12 @@
 --Travail fait par Jorge/Alejandro
-create table Coiffeur (
+	create table Coiffeur (
     iDCoiffeur INT IDENTITY(1,1),
+	UsernameCoiffeur varchar(50) unique,
     IDSalon  INT,
-    PrénomCoiffeur varchar(50) not null,
+    PrenomCoiffeur varchar(50) not null,
     NomCoiffeur  varchar(50) not null,
-    Email varchar(50) not null,
-    MotDePasse VARCHAR(100) not null,
-    PhotoCoiffeur  varchar(50)
+    PhotoCoiffeur  varchar(50),
+	MotDePasse varchar(128) not null
     CONSTRAINT PK_Coiffeur_IDCoiffeur PRIMARY KEY (iDCoiffeur),
     CONSTRAINT FK_Coiffeur_IDSalon FOREIGN KEY (idSalon) REFERENCES SalonCoiffure(idSalon)
-
-
 )
