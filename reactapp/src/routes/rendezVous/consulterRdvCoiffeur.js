3168cdedb6be608bdbc7a7b6c26db1860ef3db78
Alejandro <aleoropesa2907@gmail.com>
2024-04-01 17:44:05 -0400

    Ajout des routes de tables Rendez vous, Dispo Coiffeur et DispoSalon ainsi que organisation du projet avec ajouts des commentaires etc.

---
+++ reactapp/src/routes/rendezVous/consulterRdvCoiffeur.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/RDVCoiffeur', async (req,res) =>{
+    try{
+        const {idCoiffeur} = req.body
+
+        const verifierCoiffeur = await db.select().from('Coiffeur').where({iDCoiffeur: idCoiffeur})
+
+        if(verifierCoiffeur.length === 0){
+            return res.status(400).json({ message: 'Le coiffeur n existe pas' });
+        }
+
+        const resultats = await db('RendezVous').select().where({idCoiffeur: idCoiffeur})
+
+        res.status(200).json({ resultats });
+    } catch(error){
+        console.error("Erreur", error);
+        res.status(500).json({ error: 'Erreur lors de l consultation du rdv' });
+    }
+})
+
+module.exports = router
