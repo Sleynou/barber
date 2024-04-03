8de397a3d720663ff77aac1aec463d541348d00c
Alejandro <aleoropesa2907@gmail.com>
2024-04-02 20:57:08 -0400

    tables sql changees pour SQLITE

--- reactapp/bd/tables/Blacklist.sql
+++
@@ -1,4 +0,0 @@
-	CREATE TABLE Blacklist (
-    idToken int PRIMARY KEY identity(1,1),
-    token varchar(512) not null
-    );
--- reactapp/bd/tables/CoiffeurFavoris.sql
+++
@@ -1,6 +0,0 @@
---Travail fait par Jorge/Alejandro
-create table CoiffeurFavoris (
-    idFavoris INT PRIMARY KEY IDENTITY(1,1), 
-    idClient INT FOREIGN KEY references Client(idCLIENT), 
-    idCoiffeur INT FOREIGN KEY references Coiffeur(IDCoiffeur)
-    )
